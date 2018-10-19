955dd14f9813e16b36a5b76dc0af1dc4b95a55e5
Charlie <charlie@sigmaconnectivity.se>
2018-10-19 12:35:09 +0200

    [initial] initial commit for the project
    - This patch contains the initial project files.

---
+++ SmartHome-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartHome-ios
+//
+//  Created by Charlie Tuna on 2018-10-19.
+//  Copyright © 2018 Charlie Tuna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
